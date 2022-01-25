39f6a4a8b3d7da25162e52a1e69429d9642ba992
Yunus Inoluk <yunusinoluk@gmail.com>
2022-01-25 18:27:11 +0300

    Hamburger menu functionality added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 "use strict";
+const menu = document.querySelector(".header-menu");
+const hamburger = document.querySelector(".header-hamburger");
+const closeBtn = document.querySelector(".close");
+
+hamburger.addEventListener("click", (e) => {
+  menu.style.display = "flex";
+});
+closeBtn.addEventListener("click", (e) => {
+  menu.style.removeProperty("display");
+});
